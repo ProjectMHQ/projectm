175c3596d9e0175dc10ea5f18ef32ad97d4f04bd
gdassori <guido.dassori@gmail.com>
2019-11-03 16:58:34 +0100

    untested get\set redis ECS repo draft

--- core/src/world/repositories/data_repository.py
+++ core/src/world/repositories/data_repository.py
@@ -4,8 +4,8 @@ import bitarray
 import os
 from redis import StrictRedis
 from core.src.logging_factory import LOGGER
-from core.src.world.components import ComponentType
-from core.src.world.entity import Entity
+from core.src.world.components import ComponentType, ComponentTypeEnum
+from core.src.world.entity import Entity, EntityID
 from core.src.world.types import Bit
 
 
@@ -33,7 +33,7 @@ class RedisDataRepository:
     def save_entity(self, entity: Entity) -> Entity:
         assert not entity.entity_id, 'entity_id: %s, use update, not save.' % entity.entity_id
         entity_id = self._allocate_entity_id()
-        entity.entity_id = entity_id
+        entity.entity_id = EntityID(entity_id)
         self.update_entities(entity)
         return entity
 
@@ -52,14 +52,16 @@ class RedisDataRepository:
                 )
                 if c.has_data() and not c.has_operation():
                     LOGGER.core.debug('Absolute value, component data set')
+                    _comp_v = {entity.entity_id: c.value}
+                    _ent_v = {c.key: c.value}
                     try:
-                        components_updates[c.key].update({entity.entity_id: c.value})
+                        components_updates[c.key].update(_comp_v)
                     except KeyError:
-                        components_updates[c.key] = {entity.entity_id: c.value}
+                        components_updates[c.key] = _comp_v
                     try:
-                        entities_updates[entity.entity_id].update({c.key: c.value})
+                        entities_updates[entity.entity_id].update(_ent_v)
                     except KeyError:
-                        entities_updates[entity.entity_id] = {c.key: c.value}
+                        entities_updates[entity.entity_id] = _ent_v
                 elif c.has_data():
                     LOGGER.core.debug('Relative value, component data incr')
                     assert c.component_type == int
@@ -107,28 +109,65 @@ class RedisDataRepository:
         LOGGER.core.debug('EntityRepository.update_entity_components, response: %s', response)
         return response
 
-    def get_components_values_per_entity(
-            self, entity_id: int, *components: ComponentType
-    ) -> typing.List[typing.Optional[bytes]]:
-        response = self.redis.hmget('{}:{}'.format(self._entity_prefix, entity_id), (c.key for c in components))
-        LOGGER.core.debug(
-            'EntityRepository.get_components_values_per_entity(%s, %s), response: %s',
-            entity_id, components
-        )
-        return response
+    def get_components_values_by_entities(
+            self,
+            entities: typing.List[Entity],
+            components: typing.List[ComponentType]
+    ) -> typing.Dict[EntityID, typing.Dict[ComponentTypeEnum, bytes]]:
+        _filtered = self._get_components_statuses_by_entities(entities, components)
+        return self._get_components_values_from_entities_storage(_filtered)
 
-    def get_components_values_per_entities(
-            self, entities: typing.List[int], components: typing.List[ComponentType]
-    ) -> typing.List[typing.Optional[bytes]]:
+    def get_components_values_by_components(
+            self,
+            entities: typing.List[Entity],
+            components: typing.List[ComponentType]
+    ) -> typing.Dict[ComponentTypeEnum, typing.Dict[EntityID, bytes]]:
+        _filtered = self._get_components_statuses_by_components(entities, components)
+        return self._get_components_values_from_components_storage(_filtered)
+
+    def _get_components_statuses_by_entities(
+            self,
+            entities: typing.List[Entity],
+            components: typing.List[ComponentType]
+    ) -> typing.Dict:
         pipeline = self.redis.pipeline()
+        data_by_entity = {}
+        # FIXME TODO - Think about that.
+        for _c in components:
+            for _e in entities:
+                pipeline.getbit('{}:{}:{}'.format(self._component_prefix, _c, self._map_suffix), _e)
+        data = pipeline.execute()
+        i = 0
+        for comp in components:
+            for ent in entities:
+                _ent_v = {comp.key: data[i]}
+                try:
+                    data_by_entity[ent.entity_id].update(_ent_v)
+                except KeyError:
+                    data_by_entity[ent.entity_id] = _ent_v
+        return data_by_entity
 
-        for c in components:
-            pipeline.hmget(
-                self._component_prefix + ':' + c.key + ':' + self._data_suffix,
-                entities
-            )
-        response = pipeline.execute()
-        return response
+    def _get_components_statuses_by_components(
+            self,
+            entities: typing.List[Entity],
+            components: typing.List[ComponentType]
+    ) -> typing.Dict:
+        pipeline = self.redis.pipeline()
+        data_by_component = {}
+        for _c in components:
+            for _e in entities:  # same
+                pipeline.getbit('{}:{}:{}'.format(self._component_prefix, _c.key, self._map_suffix), _e.entity_id)
+        data = pipeline.execute()
+        i = 0
+        for comp in components:
+            for ent in entities:
+                _comp_v = {ent.entity_id: data[i]}
+                try:
+                    data_by_component[comp.key].update(_comp_v)
+                except KeyError:
+                    data_by_component[comp.key] = _comp_v
+                i += 1
+        return data_by_component
 
     def get_entity_ids_with_components(self, *components: ComponentType) -> typing.Iterator[int]:
         _key = binascii.hexlify(os.urandom(8))
@@ -138,3 +177,24 @@ class RedisDataRepository:
         self.redis.delete(_key)
         bitmap, _ = p.execute()
         return (i for i, v in enumerate(bitarray.bitarray().frombytes(bitmap)) if v)
+
+    def _get_components_values_from_components_storage(self, filtered_query: typing.Dict):
+        pipeline = self.redis.pipeline()
+        for c_key in filtered_query:
+            pipeline.hmget(
+                '{}:{}:{}'.format(self._component_prefix, c_key, self._data_suffix),
+                *(ent_id for ent_id, status in filtered_query[c_key].items() if status)
+            )
+        response = pipeline.execute()
+        return response
+
+    def _get_components_values_from_entities_storage(self, filtered_query: typing.Dict):
+        pipeline = self.redis.pipeline()
+        for entity_id in filtered_query.items():
+            pipeline.hmget(
+                '{}:{}'.format(self._entity_prefix, entity_id),
+                *(comp_key for comp_key, status in filtered_query[entity_id].items() if status)
+            )
+        response = pipeline.execute()
+        return response
+
