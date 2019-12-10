ded61e202791324ccf44386b10ea1484951fefb9
Guido Dassori <guido@conio.com>
2019-12-10 18:56:31 +0100

    enrich whoami for debugging

--- core/src/world/actions/whoami.py
+++ core/src/world/actions/whoami.py
@@ -1,10 +1,19 @@
+import json
+
+from core.src.world.components.name import NameComponent
 from core.src.world.entity import Entity
 
 
 async def whoami(entity: Entity):
-    await entity.emit_system_event(
-        {
-            "event": "whoami",
-            "id": entity.entity_id
-        }
+    from core.src.world.builder import world_repository
+    name = await world_repository.get_component_value_by_entity_id(entity.entity_id, NameComponent)
+    await entity.emit_msg(
+        json.dumps(
+            {
+                "event": "whoami",
+                "id": entity.entity_id,
+                "name": name.value
+            }
+        )
+
     )
